370f2e7dd09cdfdbdc5c3e3ef92de788ac131454
shai9 <dennis.shakava@student.moringaschool.com>
2022-11-17 11:56:59 +0300

    implement UseState and useEffect

--- client/src/components/Hotels/Hotels.js
+++ client/src/components/Hotels/Hotels.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 
 const Hotels = () => {
+  const [hotels, setHotels] = useState([])
+
+  useEffect (()=> {
+      //get all hotels from the api
+      //update all hotels in state
+      
+  })
   return (
     <div>
       This is the hotels*index view for our app
