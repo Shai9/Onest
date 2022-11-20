66a97ede3ec9f375420cdd9eee29409a501509f5
shai9 <dennis.shakava@student.moringaschool.com>
2022-11-20 23:58:20 +0300

    style the hotel component

--- client/src/components/Hotels/Hotel.js
+++ client/src/components/Hotels/Hotel.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 const Hotel = (props) => {
   return (
-    <div className=''>
-      <div className=''>
-          <img src={props.attributes.image_url}  alt={props.attributes.name}/>
+    <div className='border bg-sky-500'>
+      <div className='min-h-[50px]'>
+          <img  className='min-h-[50px] w-12 rounded border' src={props.attributes.image_url}  alt={props.attributes.name}/>
       </div>
-      <div className=''>{props.attributes.name}</div>
-      <div className=''>{props.attributes.avg_score}</div>
+      <div className='p-6'>{props.attributes.name}</div>
+      <div className='scroll-ml-1 h-1'>{props.attributes.avg_score}</div>
       <div className=''>
         <Link to={`/hotels/${props.attributes.slug}`}>View Hotel</Link>
       </div>
