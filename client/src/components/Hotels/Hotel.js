e10d5734d6ed734f3b5c057a645ba7356f16c579
shai9 <dennis.shakava@student.moringaschool.com>
2022-11-21 11:01:42 +0300

    style individual hotel

--- client/src/components/Hotels/Hotel.js
+++ client/src/components/Hotels/Hotel.js
@@ -2,20 +2,63 @@ import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import styled from 'styled-components';
 
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`
 
+const HotelLogo = styled.div`
+  height: 50px;
+  img {
+    height: 50px;
+    width: 50px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 100%;
+  }
+`
+
+const HotelName = styled.div`
+  padding: 20px 0 10px 0;
+`
+
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height:50px;
+  a {
+    color: #fff;
+    background-color: #71b406;
+    border-radius: 4px;
+    padding: 10px 50px;
+    cursor: pointer;
+    border-radius: 3px;
+    border: 1px solid #71b406;
+    text-align: center;
+    line-height: 20px;
+    min-height: 40px;
+    margin: 7px;
+    font-weight: 600;
+    text-decoration: none;
+    width: 100%;
+    transition: ease-in-out 0.1s;
+    &:hover{
+      border-color: #619a07;
+      background: #619a07;
+    }
+  }
+`
 
 const Hotel = (props) => {
   return (
-    <div className='card'>
-      <div className='hotel-logo'>
+    <Card>
+      <HotelLogo>
           <img src={props.attributes.image_url}  alt={props.attributes.name}/>
-      </div>
-      <div className='hotel-name'>{props.attributes.name}</div>
+      </HotelLogo>
+      <HotelName>{props.attributes.name}</HotelName>
       <div className='hotel-avg_score'>{props.attributes.avg_score}</div>
-      <div className='hotel-link'>
+      <LinkWrapper>
         <Link to={`/hotels/${props.attributes.slug}`}>View Hotel</Link>
-      </div>
-    </div>
+      </LinkWrapper>
+    </Card>
   )
 }
 
