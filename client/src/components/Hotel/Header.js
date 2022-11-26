97ae15b7eb1083b8ac4b0bbe7b921c82372c6061
shai9 <dennis.shakava@student.moringaschool.com>
2022-11-26 23:35:29 +0300

    test the individual hotel page

--- client/src/components/Hotel/Header.js
+++ client/src/components/Hotel/Header.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
+// import React from 'react'
+// import styled from 'styled-components'
 
-const Wrapper = styled.div`
-  padding: 50px 100px 50px 0px;
-  font-size: bold;
+// const Wrapper = styled.div`
+//   padding: 50px 100px 50px 0px;
+//   font-size: bold;
 
-  img {
-    height: 70px;
-    width: 70px;
-    border-radius: 100px;
-    border: 1px;
-    margin-bottom: -8px;
+//   img {
+//     height: 70px;
+//     width: 70px;
+//     border-radius: 100px;
+//     border: 1px;
+//     margin-bottom: -8px;
 
-`
-const TotalReviews=styled.div`
-  font-size: 18px;
-  padding: 10px 0px;
-`
-const TotalOutOf = styled.div`
-  font-size: 18px;
-  font-weight: bold;
-  padding: 10px 0px;
-`
+// `
+// const TotalReviews=styled.div`
+//   font-size: 18px;
+//   padding: 10px 0px;
+// `
+// const TotalOutOf = styled.div`
+//   font-size: 18px;
+//   font-weight: bold;
+//   padding: 10px 0px;
+// `
 
-const Header = (props) => {
+// const Header = (props) => {
 
-  const {name, image_url, avg_score} = props.attributes
-  const total = props.reviews.length
-  return (
-    <Wrapper>
-      <h1> <img src={image_url}  alt={name}/>{name}</h1>
-      <div>
-        <TotalReviews>{total} User Reviews</TotalReviews>
-        <div className='star rating'></div>
-        <TotalOutOf>{avg_score} out of 5</TotalOutOf>
-      </div>
-    </Wrapper>
-  )
-}
+//   const {name, image_url, avg_score} = props.attributes
+//   const total = props.reviews.length
+//   return (
+//     <Wrapper>
+//       <h1> <img src={image_url}  alt={name}/>{name}</h1>
+//       <div>
+//         <TotalReviews>{total} User Reviews</TotalReviews>
+//         <div className='star rating'></div>
+//         <TotalOutOf>{avg_score} out of 5</TotalOutOf>
+//       </div>
+//     </Wrapper>
+//   )
+// }
 
-export default Header
+// export default Header
--- client/src/components/Hotel/Hotel.js
+++ client/src/components/Hotel/Hotel.js
@@ -1,63 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Header from './Header'
-import styled from 'styled-components'
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import Header from './Header'
+// import styled from 'styled-components'
 
-const Wrapper = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr)
-`
-const Column = styled.div`
-  background: white;
-  height: 100vh;
-  overflow: scroll
+// const Wrapper = styled.div`
+//   margin-left: auto;
+//   margin-right: auto;
+//   display: grid;
+//   grid-template-columns: repeat(2, 1fr)
+// `
+// const Column = styled.div`
+//   background: white;
+//   height: 100vh;
+//   overflow: scroll
 
-  $last-child {
-    background: black;
-  }
-`
-const Main= styled.div`
-  padding-left: 50px;
-`
+//   $last-child {
+//     background: black;
+//   }
+// `
+// const Main= styled.div`
+//   padding-left: 50px;
+// `
 
-const Hotel = (props) => {
+// const Hotel = (props) => {
 
-    const [hotel, setHotel] = useState({})
-    const [review, setReview] = useState({})
-    const [loaded, setLoaded] = useState(false)
+//     const [hotel, setHotel] = useState({})
+//     const [review, setReview] = useState({})
+//     const [loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
-      const slug = props.match.params.slug
-      const url = `api/v1/hotels/${slug}`
-      // api/v1/hotels/ole-sereni
-      // hotels/ole-sereni
-      axios.get(url)
-      .then( resp => { 
-        setHotel(resp.data)
-        setLoaded(true)
-        })
-      .catch(resp = console.log(resp))
-    },[])
-    return(
-      <Wrapper>
-        <Column>
-          <Main>
-            {
-              loaded &&
-              <Header 
-                attributes = {hotel.data.attributes}
-                reviews = {hotel.included}
-              />
-            }
-            <div className='reviews'></div>
-          </Main>
-        </Column>
-        <div className='columns'>
-          <div className='review-form'>Review Goes Here</div>
-        </div>
-      </Wrapper>
-    )
-} 
+//     useEffect(() => {
+//       const slug = props.match.params.slug
+//       const url = `api/v1/hotels/${slug}`
+//       // api/v1/hotels/ole-sereni
+//       // hotels/ole-sereni
+//       axios.get(url)
+//       .then( resp => { 
+//         setHotel(resp.data)
+//         setLoaded(true)
+//         })
+//       .catch(resp = console.log(resp))
+//     },[])
+//     return(
+//       <Wrapper>
+//         <Column>
+//           <Main>
+//             {
+//               loaded &&
+//               <Header 
+//                 attributes = {hotel.data.attributes}
+//                 reviews = {hotel.included}
+//               />
+//             }
+//             <div className='reviews'></div>
+//           </Main>
+//         </Column>
+//         <div className='columns'>
+//           <div className='review-form'>Review Goes Here</div>
+//         </div>
+//       </Wrapper>
+//     )
+// } 
+// export default Hotel
+
+import React from 'react'
+
+const Hotel = () => {
+  return(
+     <div>
+      This is the single hotel section
+     </div>
+  )
+}
+
 export default Hotel
